3088346fe39e8d71d34dac92792d76d7f4515f94
marcos1305 <marcos1305pt@gmail.com>
2018-06-23 14:45:01 -0300

    Criando Tabelas One To Many

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,13 @@ Route::get('one-to-one', 'OneToOneController@oneToOne');
 Route::get('one-to-one-inverse', 'OneToOneController@oneToOneInverse');
 Route::get('one-to-one-insert', 'OneToOneController@oneToOneinsert');
 
+
+
+//One To Many
+Route::get('one-to-many', 'OneToManyController@oneToMany');
+
+
+
 Route::get('/', function () {
     return view('welcome');
 });
